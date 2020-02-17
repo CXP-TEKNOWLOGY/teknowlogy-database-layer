62c75528f7c5fb0f311bb31ba39c0a02d806f24e
David Courtaigne <dcourtaigne@gmail.com>
2020-02-17 08:51:08 +0100

    corrections for local testing

--- nodejs/lib/index.js
+++ nodejs/lib/index.js
@@ -1,17 +1,22 @@
 'use strict';
 const pgp = require('pg-promise')();
+const AWS = require('aws-sdk');
+const ssm = new AWS.SSM();
 
 module.exports = class DataBase {
-    constructor(connectionConfig) {
-        this.connectionConfig = connectionConfig;
-        this.open();
+    constructor() {
+        this.connectionConfig;
+        this.connection;         
     }
     async close() {
         await pgp.end();
     }
 
-    open() {
-        this.connection = pgp(this.connectionConfig);
+    async open() {
+        this.connectionConfig = await getDbParameters();
+        console.log(this.connectionConfig)
+        this.connection = await pgp(this.connectionConfig);
+        console.log(this.connection)
     }
   
     async runSQL(sql) {
@@ -19,6 +24,8 @@ module.exports = class DataBase {
     }
 
     async tekQuery(sqlQuery){
+        console.log('DB LAYER this.connection')
+        console.log(this.connection)
         return this.connection.query(sqlQuery)
     }
     /*
@@ -32,3 +39,30 @@ module.exports = class DataBase {
     }
     */
 };
+
+const getDbParameters = async () => {
+    let req = {
+        Names: [ '/DEV/PGHOST', '/DEV/PGPORT', '/DEV/PGDATABASE', '/DEV/PGUSER', '/DEV/PGPASSWORD'  ],
+        WithDecryption: true
+    }
+
+    let resp = await ssm.getParameters(req).promise()
+
+    let mapping = {
+        "/DEV/PGHOST" : "host",
+        "/DEV/PGPORT" : "port",
+        "/DEV/PGDATABASE" : "database",
+        "/DEV/PGUSER" : "user",
+        "/DEV/PGPASSWORD" : "password",
+
+    }
+
+    let params = {}
+    for (let p of resp.Parameters) {
+        mapping
+        params[mapping[p.Name]] = p.Value
+        if(p.Name == '/DEV/PGPORT') params[mapping[p.Name]] = parseInt(p.Value)
+    }
+    console.log(params)
+    return params
+}
